67a0e6296d3de274425ca3f355a6e2e94140f9bd
Corey Graves <cgraves414@gmail.com>
2020-10-24 12:13:12 -0700

    Revert "Update playerHealth.cs"
    This reverts commit b12b556ccf22f5f2eb840a532808ac9a18c9f256.

--- GameLogic/Assets/playerHealth.cs
+++ GameLogic/Assets/playerHealth.cs
@@ -16,6 +16,7 @@ public class playerHealth : MonoBehaviour
     // Update is called once per frame
     void Update() {
         if (gameObject.transform.position.y < -7) {
+            hasDied = true;
             Die();
         }
     }

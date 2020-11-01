d35675996fb8f710e6ca265e5c7139e60fa6a110
Corey Graves <cgraves414@gmail.com>
2020-11-01 11:35:44 -0800

    Raycast Method shooting
    Raycast shooting happens much quicker and can be done in an instant.
    
    There was a way to render a line that would appear and dissapear once "shoot1" was done. But wasnt include in syntax.

--- Projectiles/Assets/Scripts/weapon.cs
+++ Projectiles/Assets/Scripts/weapon.cs
@@ -5,17 +5,22 @@ using UnityEngine;
 public class weapon : MonoBehaviour
 {
 public Transform firePoint;
-public GameObject projectilePrefab;
-    // Update is called once per frame
-    void Update()
-    {
-    //Fire1 is left mouse button
-       if (Input.GetButtonDown("Fire1")) {
-          Shoot(); 
-       }
-       void Shoot() {
-           // Shoot Logic
-        Instantiate(projectilePrefab, firePoint.position, firePoint.rotation);
-       }
-    }
+public int damage = 30;
+
+void Shoot() {
+   
+   RaycastHit2D hitInfo = Physics2D.Raycast(firePoint.position, firePoint.right);
+   if (hitInfo)   {      
+      Enemy enemy = hitInfo.transform.GetComponent<Enemy>();
+      hitInfo.transform.GetComponent<Enemy>();
+      if (enemy != null)   {         
+         enemy.TakeDamage(damage);
+      }
+   }
+}
+void Update()  {
+   if (Input.GetButtonDown("Fire1"))   {
+      Shoot();
+   }   
+}
 }

fad6daa5a700f612b66065765ee6198e4e3b9577
Corey Graves <cgraves414@gmail.com>
2020-10-31 22:14:57 -0700

    Prefab Method shooting
    Prefab method shooting uses actual objects to fire. And when they fire into a collidable object they deal damage to said object.

---
+++ Projectiles/Assets/Scripts/bullet.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour
+{
+    
+    public float speed = 25f;
+    public int damage = 20;    
+    public Rigidbody2D rb;
+    
+
+    void Start()
+    {
+    // MK BULLET MOVE
+        rb.velocity = transform.right * speed;
+    }
+
+    void onTriggerEnter2D (Collider2D hitInfo) {
+        Enemy enemy = hitInfo.GetComponent<Enemy>();
+        if (enemy != null)  {
+           enemy.TakeDamage(damage); 
+        }
+        Destroy(gameObject);
+        
+    }
+}
---
+++ Projectiles/Assets/Scripts/weapon.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class weapon : MonoBehaviour
+{
+public Transform firePoint;
+public GameObject projectilePrefab;
+    // Update is called once per frame
+    void Update()
+    {
+    //Fire1 is left mouse button
+       if (Input.GetButtonDown("Fire1")) {
+          Shoot(); 
+       }
+       void Shoot() {
+           // Shoot Logic
+        Instantiate(projectilePrefab, firePoint.position, firePoint.rotation);
+       }
+    }
+}

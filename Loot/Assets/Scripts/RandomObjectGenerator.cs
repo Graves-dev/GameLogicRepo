6fcfad0652fbba4cdf01de79f5772876ccf5f133
Corey Graves <cgraves414@gmail.com>
2020-11-02 21:31:48 -0800

    RandomObjectGenerator - code
    // Changed name of GameLogic - MovementProto
    
    Created simple Random Generation code.

---
+++ Loot/Assets/Scripts/RandomObjectGenerator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomObjectGenerator : MonoBehaviour  {
+    public GameObject[] randomObject;
+
+    void Update()   {
+                
+        //Generates random  Objects
+        Instantiate (randomObject[Random.Range(0, randomObject.Length)], transform.position, transform.rotation); 
+       
+        }
+}

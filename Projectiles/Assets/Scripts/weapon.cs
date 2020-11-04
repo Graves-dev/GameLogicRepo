4a7e7646a4414d613429cab98816a8941d8c1f05
Corey Graves <cgraves414@gmail.com>
2020-11-04 13:38:07 -0800

    ~Files Moved~

---
+++ Projectiles/Assets/Scripts/weapon.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class weapon : MonoBehaviour
+{
+public Transform firePoint;
+public int damage = 30;
+
+void Shoot() {
+   
+   RaycastHit2D hitInfo = Physics2D.Raycast(firePoint.position, firePoint.right);
+   if (hitInfo)   {      
+      Enemy enemy = hitInfo.transform.GetComponent<Enemy>();
+      hitInfo.transform.GetComponent<Enemy>();
+      if (enemy != null)   {         
+         enemy.TakeDamage(damage);
+      }
+   }
+}
+void Update()  {
+   if (Input.GetButtonDown("Fire1"))   {
+      Shoot();
+   }   
+}
+}
